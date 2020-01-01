3c4b2abbc5622fd13ef39ea10c4a2cdfbbe2cdf7
ddouble <ddouble.cn@gmail.com>
2020-01-01 23:20:09 +0800

    implement main page framework which including top nav & siderbar for admin site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import BasicLayout from "./common/layouts/BasicLayout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <BrowserRouter>
+      <Switch>
+
+        <Route path="/login">
+          <Login/>
+        </Route>
+
+        <Route path="/">
+          <BasicLayout>
+            <Switch>
+              <Route path="/dashboard">
+                <Dashboard/>
+              </Route>
+              <Route path="/merchants">
+                <Merchants/>
+              </Route>
+              <Route path="/users">
+                <Users/>
+              </Route>
+
+              {/*default route*/}
+              <Route path="/">
+                <Redirect to={'/dashboard'}/>
+              </Route>
+
+            </Switch>
+          </BasicLayout>
+        </Route>
+
+
+      </Switch>
+    </BrowserRouter>
+  </div>;
+}
+
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function Merchants() {
+  return <h2>Merchants</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
