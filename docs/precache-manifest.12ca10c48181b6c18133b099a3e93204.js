8472a96e47ca569f574ea3df7c6cd5a71b5fc167
ddouble <ddouble.cn@gmail.com>
2021-08-30 00:00:47 +0200

    Publish demo

---
+++ docs/precache-manifest.12ca10c48181b6c18133b099a3e93204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab722739f3b3352bbf354bfa456fc480",
+    "url": "/cra/index.html"
+  },
+  {
+    "revision": "f76e92d6158e2afbac1c",
+    "url": "/cra/static/css/main.5538a3c3.chunk.css"
+  },
+  {
+    "revision": "b0f57f83c25ea4719f84",
+    "url": "/cra/static/js/2.926d1c73.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/cra/static/js/2.926d1c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e7a75258cc2f80881b",
+    "url": "/cra/static/js/3.db27ad50.chunk.js"
+  },
+  {
+    "revision": "f76e92d6158e2afbac1c",
+    "url": "/cra/static/js/main.028e2f42.chunk.js"
+  },
+  {
+    "revision": "8928a8ae8d9540b78c1b",
+    "url": "/cra/static/js/runtime-main.33438aae.js"
+  }
+]);
